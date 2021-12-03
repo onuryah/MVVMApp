8235cedd6e1bf3f26497de2b4cb22dd434018f6c
Onuralp <xonuraralpx@gmail.com>
2021-12-03 23:16:25 +0300

    Initial Commit

---
+++ MVVMApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MVVMApp
+//
+//  Created by Ceren Çapar on 3.12.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
